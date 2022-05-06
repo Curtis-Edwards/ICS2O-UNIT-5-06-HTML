f766e2bd15d75c8f096c8d072ee34af48797e796
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-06 13:18:38 -0400

    added flowchart

--- js/script.js
+++ js/script.js
@@ -8,12 +8,26 @@
  * Check service worker.
  */
  if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-PWA-Template/sw.js", {
-    scope: "/ICS20-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-UNIT-5-06-HTML/sw.js", {
+    scope: "/ICS2O-UNIT-5-06-HTML/",
   })
 }
 
 /**
  * Input.
  */
+
+function buttonClicked() {
+
+  const integer1 = parseInt(document.getElementById('integer1').value)
+  var integer2 = parseInt(document.getElementById('integer2').value)
+  var counter = 0
+  var answer = 0
+
+  while (counter != integer2 || counter < integer2) {
+    answer + integer1
+    counter++
+  }
+  document.getElementById("output").innerHTML = answer
+}
 
