f2f6ab56ba24c00603e7798b5ba4294b6c2a4eee
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-07 14:26:18 -0400

    added comment

--- js/script.js
+++ js/script.js
@@ -23,17 +23,24 @@ function buttonClicked() {
   var integer2 = parseInt(document.getElementById('integer2').value)
   var counter = 0
   var answer = 0
-
-  if (integer1 != 0 && integer2 >= 0) {
+// if both numbers are positive
+  if (intger1 > 0 && integer2 > 0) {
+    while (counter < integer2) {
+      answer = answer + integer1
+      counter = counter + 1
+    }
+// if one of the numbers is positive
+  } else if (integer1 > 0 && integer2 < 0) {
     while (counter < integer2) {
       answer = answer + integer1
       counter = counter + 1
     } 
-  } else if (integer2 != 0 && integer1 >= 0) {
+  } else if (integer1 < 0 && integer2 > 0) {
       while (counter < integer1) {
         answer = answer + integer2
         counter = counter + 1
     }
+// if both number are negative
   } else if (integer2 < 0 && integer1 < 0) {
       while (counter > integer2) {
         answer = answer - integer1
