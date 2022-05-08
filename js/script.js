b44d38d8a4eec65803a157a05d082b2b0a0bf8b9
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-07 20:23:43 -0400

    fixed spelling

--- js/script.js
+++ js/script.js
@@ -33,12 +33,12 @@ function buttonClicked() {
   } else if (integer1 > 0 && integer2 < 0) {
     while (counter < integer2) {
       answer = answer - integer1
-      counter = counter + 1
+      counter = counter - 1
     } 
   } else if (integer1 < 0 && integer2 > 0) {
       while (counter < integer1) {
         answer = answer - integer2
-        counter = counter + 1
+        counter = counter - 1
     }
 // if both number are negative
   } else if (integer2 < 0 && integer1 < 0) {
